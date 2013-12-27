b07ffd37ef17aad9860d5c706a3ad954f0089b46
Lukas <lukas.valatka123@gmail.com>
2013-12-27 13:46:05 +0200

    Email are not case sensitive anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
 
 
-validates :username,  presence: true, length: { maximum: 10 }
+validates :username,  presence: true, length: { maximum: 10 }, uniqueness: true
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+                    uniqueness: { case_sensitive: false }
 
-validates :password,  presence: true, length: { maximum: 50 }  
+validates :password,  presence: true, length: { maximum: 50 }
   
 end
