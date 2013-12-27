ec88d97f89adc1b2e686bf3046a2762a87389c90
Lukas <lukas.valatka123@gmail.com>
2013-12-27 20:19:29 +0200

    User model password encrypting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,32 @@
 class User < ActiveRecord::Base
+before_save :encrypt_password
+after_save :clear_password
 
 
-validates :username,  presence: true, length: { maximum: 10 }, uniqueness: true
+
+
+def encrypt_password
+
+salt = BCrypt::Engine.generate_salt
+encrypted_password = BCrypt::Engine.hash_secret(password, salt)
+
+end
+
+
+def clear_password
+
+
+end
+
+validates :username,  presence: true, length: { :in => 1..15 }, uniqueness: true
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-validates :password,  presence: true, length: { maximum: 50 }
+validates :password,  presence: true, length: { :in => 6..20 }
+
+attr_accessible :username, :email, :password
   
 end
